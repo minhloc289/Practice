739e674cd54b9b21b62763256d7d2c8eb7f9c889
minhloc289 <121540337+minhloc289@users.noreply.github.com>
2023-03-27 23:04:20 +0700

    Update đảo mảng đặc biệtcpp.cpp

--- đảo mảng đặc biệtcpp.cpp
+++ đảo mảng đặc biệtcpp.cpp
@@ -1,53 +1,70 @@
 #include <iostream>
-
 using namespace std;
-
-void inputArray(int a[], int& n) {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-}
-void printArray(int a[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+void Nhap(int a[], int& n) {
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
 }
 
-void reverseArray(int a[], int n) {
-    int left = 0;
-    int right = n - 1;
-    int x = a[(left + right) / 2];
-    int i = left, j = right;
-    while (i <= j) {
-        while (a[i] < x) {
-            i++;
-        }
-        while (a[j] > x) {
-            j--;
-        }
-        if (i <= j) {
-            swap(a[i], a[j]);
-            i++;
-            j--;
-        }
-    }
+void sort(int a[], int l, int r) {
+	int i = l, j = r;
+	int x = a[(l + r) / 2];
+	while (i <= j) {
+		while (a[i] < x) {
+			i++;
+		}
+		while (a[j] > x) {
+			j--;
+		}
+		if (i <= j) {
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}
+	if (l < j)
+		sort(a, l, j);
+	if (i < r)
+		sort(a, i, r);
 }
-int main()
-{
-    int a[100], n;
-    inputArray(a, n);
-
-    // In mang ban dau
-    printArray(a, n);
-
-    //Dao mang
-    reverseArray(a, n);
-
-    // In mang sau khi dao
-    cout << "Reversed array is" << endl;
-    printArray(a, n);
-
-    return 0;
+void EvenOddSort(int a[], int n) {
+	int nb = 0, nc = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] % 2 == 0) {
+			nc++;
+		}
+		else {
+			nb++;
+		}
+	}
+	int* b = new int[nc];
+	int* c = new int[nb];
+	int j = 0, k = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] % 2 == 0) {
+			c[j] = a[i];
+			j++;
+		}
+		else {
+			b[k] = a[i];
+			k++;
+		}
+	}
+	sort(b, 0, nb - 1);
+	sort(c, 0, nc - 1);
+	for (int i = 0; i < nb + nc; i++) {
+		if (i < nc) {
+			cout << c[i] << endl;
+		}
+		if (i >= nc) {
+			cout << b[i - nc] << endl;
+		}
+	}
+}
+int main() {
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	Nhap(a, n);
+	EvenOddSort(a, n);
 }
